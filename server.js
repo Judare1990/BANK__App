205f4c82682aa03031ed2c014fd74690d6c61302
Judare85 <judare85@hotmail.com>
2023-05-02 18:55:26 -0500

    Last commit

--- server.js
+++ server.js
@@ -2,12 +2,10 @@ require('dotenv').config();
 const app = require('./app');
 const { db } = require('./database/config');
 
-//LA AUTENTICACIÓN CON LA BASE DE DATOS
 db.authenticate()
   .then(() => console.log('Database Authenticated ✔'))
   .catch((error) => console.log(error));
 
-//LA SINCRONIZACIÓN CON LA BASE DE DATOS
 db.sync()
   .then(() => console.log('Database Synced ✔'))
   .catch((error) => console.log(error));
